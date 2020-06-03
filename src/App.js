d1c08d80a7343c7aaab555432f10a359d107ddc8
Ann-MarieKemp <aedalenb@gmail.com>
2020-06-03 13:09:51 -0400

    basic setup finished, fixing bugs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import APIKey from '../src/secrets.js';
 
 class App extends Component {
   constructor() {
     super();
     this.turnOnHandler = this.turnOnHandler.bind(this);
     this.turnOffHandler = this.turnOffHandler.bind(this);
+    this.state = {
+      pin: 0,
+    };
   }
-  async turnOnHandler() {
+  componentDidMount(color) {
+    let pins = [5, 6, 9];
+    pins.forEach(async (pinNumber) => {
+      await axios.post(
+        `http://us01.proxy.teleduino.org/api/1.0/328.php?k=${APIKey}&r=definePinMode&pin=${pinNumber}&mode=1`
+      );
+    });
+  }
+  async turnOnHandler(color) {
+    if (color === 'red') {
+      this.setState({ pin: 5 });
+    } else if (color === 'blue') {
+      this.setState({ pin: 6 });
+    } else if (color === 'green') {
+      this.setState({ pin: 9 });
+    }
     console.log('this is turn on');
-    let res = await axios.GET(
-      'https://us01.proxy.teleduino.org/api/1.0/328.php?k={BF9599700F9E8D3D63C858A32A96D9EB}&r=getVersion'
+    let res = await axios.post(
+      `http://us01.proxy.teleduino.org/api/1.0/328.php?k=${APIKey}&r=setDigitalOutput&pin=${this.state.pin}&output=1`
     );
     console.log(res);
   }
-  async turnOffHandler() {
+
+  async turnOffHandler(color) {
+    if (color === 'red') {
+      this.setState({ pin: 5 });
+    } else if (color === 'blue') {
+      this.setState({ pin: 6 });
+    } else if (color === 'green') {
+      this.setState({ pin: 9 });
+    }
     console.log('this is turn off');
+    let res = await axios.post(
+      `http://us01.proxy.teleduino.org/api/1.0/328.php?k=${APIKey}&r=setDigitalOutput&pin=${this.state.pin}&output=0`
+    );
+    console.log(res, 'output');
   }
   render() {
     return (
       <div className="App">
-        <button onClick={this.turnOnHandler}>Turn On</button>
-        <button onClick={this.turnOffHandler}>Turn Off</button>
+        <button onClick={() => this.turnOnHandler('red')}>Turn On Red</button>
+        <button onClick={() => this.turnOnHandler('blue')}>Turn On Blue</button>
+        <button onClick={() => this.turnOnHandler('green')}>
+          Turn On Green
+        </button>
+        <button onClick={() => this.turnOffHandler('red')}>Turn Off Red</button>
+        <button onClick={() => this.turnOffHandler('blue')}>
+          Turn Off Blue
+        </button>
+        <button onClick={() => this.turnOffHandler('green')}>
+          Turn Off Green
+        </button>
       </div>
     );
   }
