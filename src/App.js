196399938968bd6041eb54be8e36314ec835dc2a
Ann-MarieKemp <aedalenb@gmail.com>
2020-06-03 14:43:37 -0400

    added on/off for all three color sets, added random color option, added turn all off option

--- src/App.js
+++ src/App.js
@@ -8,8 +8,12 @@ class App extends Component {
     super();
     this.turnOnHandler = this.turnOnHandler.bind(this);
     this.turnOffHandler = this.turnOffHandler.bind(this);
+    this.pinColor = this.pinColor.bind(this);
+    this.turnOnColorAndValue = this.turnOnColorAndValue.bind(this);
+    this.turnOffAll = this.turnOffAll.bind(this);
     this.state = {
-      pin: 0,
+      color: 'red',
+      value: '255',
     };
   }
   componentDidMount(color) {
@@ -20,50 +24,90 @@ class App extends Component {
       );
     });
   }
-  async turnOnHandler(color) {
+  pinColor(color) {
+    let pin = 0;
     if (color === 'red') {
-      this.setState({ pin: 5 });
+      console.log('inredoff', pin);
+      pin = 5;
     } else if (color === 'blue') {
-      this.setState({ pin: 6 });
+      pin = 6;
+      console.log('inblueoff', pin);
     } else if (color === 'green') {
-      this.setState({ pin: 9 });
+      pin = 9;
+      console.log('ingreenoff', pin);
     }
-    console.log('this is turn on');
+    return pin;
+  }
+  async turnOnHandler(color) {
+    let pin = this.pinColor(color);
+    console.log('this is turn on', color, pin);
     let res = await axios.post(
-      `http://us01.proxy.teleduino.org/api/1.0/328.php?k=${APIKey}&r=setDigitalOutput&pin=${this.state.pin}&output=1`
+      `http://us01.proxy.teleduino.org/api/1.0/328.php?k=${APIKey}&r=setDigitalOutput&pin=${pin}&output=1`
     );
     console.log(res);
   }
 
   async turnOffHandler(color) {
-    if (color === 'red') {
-      this.setState({ pin: 5 });
-    } else if (color === 'blue') {
-      this.setState({ pin: 6 });
-    } else if (color === 'green') {
-      this.setState({ pin: 9 });
-    }
-    console.log('this is turn off');
+    let pin = this.pinColor(color);
+    console.log('this is turn off', color, pin);
     let res = await axios.post(
-      `http://us01.proxy.teleduino.org/api/1.0/328.php?k=${APIKey}&r=setDigitalOutput&pin=${this.state.pin}&output=0`
+      `http://us01.proxy.teleduino.org/api/1.0/328.php?k=${APIKey}&r=setDigitalOutput&pin=${pin}&output=0`
     );
     console.log(res, 'output');
   }
+  async turnOnColorAndValue(color, value) {
+    let pin = this.pinColor(color);
+    await axios.post(
+      `http://us01.proxy.teleduino.org/api/1.0/328.php?k=${APIKey}&r=setPwmOutput&pin=${pin}&output=${value}`
+    );
+  }
+  turnOnRandom() {
+    let colors = ['red', 'green', 'blue'];
+    let randomColor = colors[Math.floor(Math.random() * 3)];
+    console.log(randomColor);
+    let randomValue = Math.floor(Math.random() * 255);
+    console.log(randomValue);
+    this.turnDownHandler(randomColor, randomValue);
+  }
+  turnOffAll() {
+    this.turnOffHandler('blue');
+    this.turnOffHandler('green');
+    this.turnOffHandler('red');
+  }
+
   render() {
     return (
       <div className="App">
-        <button onClick={() => this.turnOnHandler('red')}>Turn On Red</button>
-        <button onClick={() => this.turnOnHandler('blue')}>Turn On Blue</button>
-        <button onClick={() => this.turnOnHandler('green')}>
+        <button className="red" onClick={() => this.turnOnHandler('red')}>
+          Turn On Red
+        </button>
+        <button className="blue" onClick={() => this.turnOnHandler('blue')}>
+          Turn On Blue
+        </button>
+        <button className="green" onClick={() => this.turnOnHandler('green')}>
           Turn On Green
         </button>
-        <button onClick={() => this.turnOffHandler('red')}>Turn Off Red</button>
-        <button onClick={() => this.turnOffHandler('blue')}>
+        <button className="red" onClick={() => this.turnOffHandler('red')}>
+          Turn Off Red
+        </button>
+        <button className="blue" onClick={() => this.turnOffHandler('blue')}>
           Turn Off Blue
         </button>
-        <button onClick={() => this.turnOffHandler('green')}>
+        <button className="green" onClick={() => this.turnOffHandler('green')}>
           Turn Off Green
         </button>
+        <button onClick={this.turnOnRandom}>Turn On Random Color</button>
+        <button onClick={this.turnOffAll}>Turn Off All Colors</button>
+        <form>
+          <select id="colors">
+            Color
+            <option value="red">red</option>
+            <option value="green">green</option>
+            <option value="blue">blue</option>
+          </select>
+          <label>Value</label>
+          <input type="number" />
+        </form>
       </div>
     );
   }
